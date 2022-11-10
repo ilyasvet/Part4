807ff38e567fefddd8d725a23ba366c7375329a6
Шемаев Кирилл <shemaev.kirill@yandex.ru>
2022-11-10 21:28:24 +0300

    Indexer

---
+++ Expansion/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expansion
+{
+    interface IIndexer
+    {
+        Person this[DateTimeKind kind] { get; set; } //параметры любого типа
+        Person this[DateTime time] { get; set; }
+        Person this[Person person] { get; set; }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surame { get; set; }
+        public int Age { get; set; }
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Age = age;
+            Surame = surname;
+        }
+    }
+    class ListOfPerson: IIndexer
+    {
+        private List<Person> list;
+        public ListOfPerson(int c)
+        {
+            list = new List<Person>(c);
+        }
+        public Person this[int index]
+        {
+            get { return list[index]; }
+            set { list.Insert(index, value); }
+        }
+        public Person this[string surname]
+        {
+            get => list.FirstOrDefault(x => x.Surame == surname);
+            set 
+            {
+                list[list.IndexOf(list.FirstOrDefault(x => x.Surame == surname))] = value;
+            }
+        }
+        //не имеет смысла, просто ради проверки возможности
+        public Person this[DateTimeKind kind]
+        {
+            get
+            {
+                if (kind == DateTimeKind.Utc)
+                {
+                    return list[0];
+                }
+                return list[1];
+            }
+            set => throw new NotImplementedException();
+        }
+        public Person this[DateTime time]
+        {
+            get
+            {
+                if (time == DateTime.MinValue)
+                {
+                    return list[1];
+                }
+                return list[2];
+            }
+            set => throw new NotImplementedException();
+        }
+        public Person this[Person person]
+        {
+            get => list.First();
+            set => list.Add(person);
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyPeople = new ListOfPerson(10);
+            MyPeople[0] = new Person("Kirill", "Kulyai", 113);
+            MyPeople[1] = new Person("Vasya", "Pupkin", 112);
+            MyPeople[2] = new Person("Petya", "Krilov", 114); //Вставка по индексу нового человека 
+            Console.WriteLine(MyPeople["Kulyai"].Name);
+            MyPeople["Pupkin"] = new Person("Pep", "Ily", 114); //Заменяем пупкина на нового 
+            Console.WriteLine(MyPeople["Ily"].Name);
+            Console.WriteLine(MyPeople[DateTimeKind.Utc].Name);
+
+            Console.WriteLine(MyPeople[DateTime.MinValue].Name);
+            var person = new Person("vas", "pes", 17);
+            MyPeople[person] = null; //На самом деле тут добавляется person в список 
+            Console.WriteLine(MyPeople[3].Name);
+        }
+    }
+}
---
+++ Expansion/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Expansion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Expansion")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("6269fb40-1c2a-4f19-bab0-c75aa15e9f3b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
