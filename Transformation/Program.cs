ebf6efa60da38d6bc58b8c94205ab409f1e0a3d0
Шемаев Кирилл <shemaev.kirill@yandex.ru>
2022-11-11 10:28:46 +0300

    transformation

---
+++ Transformation/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transformation;
+
+namespace Transformation
+{
+    public struct Rectangle
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle(int w, int h) : this() //необходимо для инициализации поддерживающих полей для автосвойств
+        {
+            Width = w; Height = h;
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < Height; i ++)
+            {
+                for (int j = 0; j < Width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static implicit operator Rectangle(Square s)
+        {
+            return new Rectangle(s.Length, s.Length * 2);
+        }
+        public override string ToString() => $"[Width = {Width}; Height = {Height}]";
+}
+    public struct Square
+    {
+        public int Length { get; set; }
+        public Square(int i) : this()
+        {
+            Length = i;
+        }
+        public void Draw()
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                for (int j = 0; j < Length; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        public override string ToString() => $"[Length] = {Length}";
+        // Rectangle можно явно преобразовывать в Square,
+        public static explicit operator Square(Rectangle r)
+        {
+            Square s = new Square { Length = r.Height };
+            return s;
+        }
+        public static explicit operator Square(int i)
+        {
+            Square s = new Square(i);
+            return s;
+        }
+        public static explicit operator int(Square s)
+        {
+            return s.Length;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10,2);
+            Console.WriteLine(r);
+            r.Draw();
+            DrawSquare((Square)r);
+            DrawSquare((Square)10); //Преобразовываем int в квадрат
+            Console.WriteLine((int)(Square)r); //И наоборот
+
+            Square s = new Square(10);
+            Rectangle rr = (Rectangle)s; //Явное, но определено только неявное.
+            Rectangle rrr = s; // Неявное. 
+            Console.WriteLine(rr);
+            Console.WriteLine(rrr);
+        }
+        static void DrawSquare(Square sq)
+        {
+            Console.WriteLine(sq);
+            sq.Draw();
+        }
+    }
+}
---
+++ Transformation/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Transformation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Transformation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("51cf3a3f-3077-4a13-b25a-3884d3e6ac2c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
